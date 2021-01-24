cd98ed26d76a057b713c45eff572c73f52d67cbf
jay kim <kimjingo@gmail.com>
2021-01-23 23:03:25 -0800

    add options with error

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {	// state 설정
-      option : 9, // or 19
+      ptype : 0, // 0:multipliaction, 1:division
+      difficulty : 9, // or 19
       first: Math.ceil(Math.random() * 9),
       second: Math.ceil(Math.random() * 9),
-      value: '',
       result:'',
+      value: '',
       history: [],
     };
   }
@@ -48,6 +49,7 @@ class App extends Component {
         history:new_history,
       });
     }
+    this.input.focus();
   };
 
   onChange = (e) => this.setState({value:e.target.value});
@@ -71,15 +73,36 @@ class App extends Component {
       );
       i=i+1;
     }
-      //      {data[i][2]} :  
+      // input;
 
     return (
       <div className="App">
         <div>
-          {this.state.first} X {this.state.second}
+            <input type="radio" name="problemtype" id="multi" value='9' checked={this.props.ptype===9}  />
+            <label htmlFor="multi">Multiplication</label>&nbsp;&nbsp;&nbsp;
+            <input type="radio" name="problemtype" id="divi" value='19' checked={this.props.ptype===19} onChange={function(e){
+                // e.preventDefault();
+                this.props.onChangeOption(19);
+            }.bind(this)}/>
+            <label htmlFor="divi">Division</label>
+        </div>
+        <div>
+            <input type="radio" name="difficulty" id="nine" value='9' checked={this.state.difficulty===9} onChange={function(e){
+                // e.preventDefault();
+                this.state.onChangeOption(9);
+            }.bind(this)}/>
+            <label htmlFor="nine">9 x 9</label>&nbsp;&nbsp;&nbsp;
+            <input type="radio" name="difficulty" id="nineteen" value='19' checked={this.state.difficulty===19} onChange={function(e){
+                // e.preventDefault();
+              this.props.onChangeOption(19);
+              }.bind(this)}/>
+            <label htmlFor="nineteen">19 x 19</label>
+        </div>
+        <div>
+          {this.state.first} X {this.state.second} = ?
         </div>
         <form onSubmit={this.onSubmit}>
-          <input style={{width:"100px"}} type="number" autoFocus onFocus={this.handleFocus} value={this.state.value} onChange={this.onChange}></input>
+          <input ref={(c)=>{this.input =c;}} style={{width:"100px"}} type="number" autoFocus onFocus={this.handleFocus} value={this.state.value} onChange={this.onChange}></input>
           <button>Submit</button>
         </form>
         <div>
